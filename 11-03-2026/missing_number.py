35cb637a14a1a73c505c1281f1763af4160a872a
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-03-11 21:51:03 +0530

    add : [Pallavi] - added code to find missing number

---
+++ 11-03-2026/missing_number.py
@@ -0,0 +1,21 @@
+"""
+Find the missing number 
+Input: arr[] = [8, 2, 4, 5, 3, 7, 1]
+Output: 6
+Explanation: All the numbers from 1 to 8 are present except 6.
+
+Input: arr[] = [1, 2, 3, 5]
+Output: 4
+Explanation: Here the size of the array is 4, so the range will be [1, 5]. The missing number between 1 to 5 is 4
+"""
+
+class Solution:
+    def missing_number(self,nums):
+        sum = 0
+        for i in nums : 
+            sum = sum + i
+
+        n= len(nums)
+        actual_sum = ((n+1)*(n+2))/2 
+
+        return actual_sum - sum 
