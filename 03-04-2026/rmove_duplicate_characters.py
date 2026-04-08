8b021d9fef57d5348687fde1708e743af4b62c05
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-04-08 11:19:25 +0530

    Add Program : [pallavi] - Added code to remove duplicate characters from a given string

---
+++ 03-04-2026/rmove_duplicate_characters.py
@@ -0,0 +1,10 @@
+'''Remove duplicate characters from a given string'''
+
+def remove_duplicates(s):
+    result = ""
+    for char in s:
+        if char not in result:
+            result += char
+    return result
+
+print(remove_duplicates("programming"))
