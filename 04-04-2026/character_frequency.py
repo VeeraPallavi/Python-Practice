02e135a2269063f4995e9e688a9c6a5df2ac8272
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-04-08 11:38:56 +0530

    Add Program : [pallavi] - Added code to count frequency of each character

---
+++ 04-04-2026/character_frequency.py
@@ -0,0 +1,9 @@
+'''Count frequency of each character.'''
+
+def char_frequency(s):
+    freq = {}
+    for char in s:
+        freq[char] = freq.get(char, 0) + 1
+    return freq
+
+print(char_frequency("hello"))
