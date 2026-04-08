efb57f0aec8722841cc3494ac606d8f13272ecf2
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-04-08 11:18:20 +0530

    Add Program : [pallavi] - Added code to count_vowels_consonants.py

---
+++ 03-04-2026/count_vowels_consonants.py
@@ -0,0 +1,16 @@
+'''count vowels and consonants in a given string'''
+
+def count_vowels_consonants(s):
+    vowels = "aeiouAEIOU"
+    v_count = c_count = 0
+    
+    for char in s:
+        if char.isalpha():
+            if char in vowels:
+                v_count += 1
+            else:
+                c_count += 1
+                
+    return v_count, c_count
+
+print(count_vowels_consonants("Hello World"))
