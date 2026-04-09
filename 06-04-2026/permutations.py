4dc71fa48dcb1b0952a09535bb764dae2de89a24
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-04-09 21:53:41 +0530

    Add Program [pallavi] - Added code to generate all permutation of a given string

---
+++ 06-04-2026/permutations.py
@@ -0,0 +1,8 @@
+'''Generate all permutations of a string'''
+
+from itertools import permutations
+
+def string_permutations(s):
+    return [''.join(p) for p in permutations(s)]
+
+print(string_permutations("abc"))
