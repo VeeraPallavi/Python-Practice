207ccb233942ef7b0b6bf77b4e77e3c1f6b88749
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-04-13 16:10:14 +0530

    Add Program : [pallavi] - Added code to find all elments appearing twice

---
+++ 08-04-2026/all_duplicates.py
@@ -0,0 +1,16 @@
+'''Find all elements appearing twice'''
+
+def find_duplicates(nums):
+    result = []
+    
+    for num in nums:
+        index = abs(num) - 1
+        
+        if nums[index] < 0:
+            result.append(abs(num))
+        else:
+            nums[index] = -nums[index]
+    
+    return result
+
+print(find_duplicates([4,3,2,7,8,2,3,1]))
